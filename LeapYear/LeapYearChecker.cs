e016b93cce88a267ef7e291d18fa74b2b6bd7e8c
Smyrechynskyi <41690469+Smyrechynskyi@users.noreply.github.com>
2021-12-02 10:37:33 +0200

    Add implementation for LeapYearChecker

---
+++ LeapYear/LeapYearChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TDD
+{
+    internal class LeapYearChecker
+    {
+        public bool CheckIfLeapYear(string stringYear)
+        {
+            var year = Int64.Parse(stringYear);
+
+            return year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0);
+        }
+    }
+}

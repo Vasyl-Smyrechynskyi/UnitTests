14f46cb1d7fd67b30454902e85b1a208d916fd0b
Smyrechynskyi <41690469+Smyrechynskyi@users.noreply.github.com>
2021-12-02 10:48:12 +0200

    Add project BerlinClock

---
+++ BerlinClock/BerlinClockConverterTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BerlinClock
+{
+    public class BerlinClockConverterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
